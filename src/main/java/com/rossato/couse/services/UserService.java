c0dd13f4244f04a2f7aad910421942342c74de90
thiagohenriquerossato <thiago.rossato.tr@gmail.com>
2023-09-01 17:34:04 -0300

    error handling

--- src/main/java/com/rossato/couse/services/UserService.java
+++ src/main/java/com/rossato/couse/services/UserService.java
@@ -1,12 +1,14 @@
 package com.rossato.couse.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.rossato.couse.entities.User;
 import com.rossato.couse.repositories.UserRepository;
+import com.rossato.couse.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -19,7 +21,8 @@ public class UserService {
   }
   
   public User findById(Long id) {
-    return repository.findById(id).get();
+    Optional<User> obj = repository.findById(id);
+    return obj.orElseThrow(() -> new ResourceNotFoundException(id));
   }
 
   public User insert(User obj) {
